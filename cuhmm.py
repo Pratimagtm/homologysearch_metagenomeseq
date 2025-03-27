1bc245ab6209fde627d6b5475620d8b2b41baef7
Pratimagtm <43498642+Pratimagtm@users.noreply.github.com>
2025-03-27 11:21:22 -0700

    Add files via upload

---
+++ cuhmm.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 24 18:51:29 2024
+
+@author: pratima
+"""
+import os
+import json
+from os import path
+import subprocess
+
+def call_hmmer(hmmscan_arg):
+    
+    print('---- Start call hmmer, Creating .tab file ----')
+    subprocess.call(hmmscan_arg, stdout=open(os.devnull, 'w'))
+    print('----Call hmmer Ended----')
+    #read tab file and create a list of TF potein accession.
+    
+    #select_TF(hmmoutputfile, samplelist)
+    
+    
+def run_hmmer(hmminputfile, eval, DB_path):
+   #print(eval, DB_path, hmminputfile)
+
+    print('---start run hmmer---')
+    hmmscan_arg = (['hmmscan','--tblout', hmmoutputfile, '-E', str(eval), \
+                    str(DB_path), hmminputfile])
+    print('---Run hmmr ended---')
+    call_hmmer(hmmscan_arg) 
+
+
+# open and read json file
+inputfilename = "input.json"
+#fastapath= "/reads_trans"
+with open('input.json') as json_file:
+    data = json.load(json_file)
+    
+# create a variable to hold dbPath value for later use.
+eval = data["Hmmer"]["Max_eval"] 
+DB_path = data["Hmmer"]["DB_path"]
+
+#print(eval, DB_path)
+
+readpath = data["Hmmer"]["Reads"]
+isfile = os.path.exists(readpath)
+
+fastalist=[]
+for f in os.listdir(readpath):
+    if f.endswith(".fasta"):
+        fastalist.append(f)
+#print(readpath, isfile)
+#print(fastalist)
+
+for file in fastalist:
+    print(file)
+    hmminputfile = readpath+file
+    hmmoutputfile= file+ ".tab"
+    run_hmmer(hmminputfile, eval, DB_path) 
+
+
+
+print(str(hmminputfile))
